2d56398a3a3e0cd0196755fb7442e1364eb239a8
Creative 3 <creative3@CTI004189.local>
2020-03-13 11:54:16 -0400

    Commit 12 - started HTML output, added city capitalization + bold, changed replacement script to output only vs each textarea. added img link in html

--- js/script.js
+++ js/script.js
@@ -6,9 +6,11 @@
 # Tab Control
 # CTA function
   ## Validator
-  ## Replacement function
+  ## Replacement functions
   ## Mailing ID
   ## Output
+    ### HTML
+    ### TXT
 # Back Button
 --------------------------------------------------------------*/
 $('#province').each(function(){
@@ -56,7 +58,7 @@ $('#btnTxt').click(function(){
 --------------------------------------------------------------*/
 $('#btnGenerate').click(function(e){
   var name = $('#name').val();
-  var img_name = $('#img_name').val();
+  var img_name = $('#img_name').val().toUpperCase();
   var presale = $('#presale').val();
   var location = $('#location').val();
 
@@ -81,14 +83,19 @@ $('#btnGenerate').click(function(e){
   var contact_us_fr = 'https://www.americanexpress.com/ca/fr/service-clientele/nous-joindre.html';
   var unsubscribe_fr = 'https://ebm.cheetahmail.com/r/regf2?a=0&aid=268935401&n=116&email=%25%25email%25%25';
 
+  // if img name is blank, give it the FPO name
+  if ( img_name == '' ){ img_name = 'AMEX_img_FPO.jpg'; }
+  var img_link = '<img src="http://f.email.americanexpress.com/i/45/268935401/' + img_name + '" alt="" title="" height="auto" style="border:none;border-radius:;display:block;outline:none;text-decoration:none;width:100%;height:auto;" width="620" />';
+
   // Terms & conditions
   var reg_en = "*Purchase must be charged in full to an American Express Card. Subject to availability and to event and ticketing terms, restrictions, verification procedures and fees. Tickets and packages may not be transferable and should not be resold. No refunds and no exchanges subject to merchant's obligations under applicable law.\n\n® Used under license from American Express. Copyright ©: AMEX Bank of Canada, 2020.\n\nSent from Amex Bank of Canada, P.O. Box 3204, Station 'F', Toronto, ON, M1W 3W7. You can contact us or verify with us the authenticity of this email by visiting: " + contact_us_en + ".\n\nNote that this e-mail was sent to: %%EMAIL%%. If you received this email at a different address, this email message was forwarded.\n\nTo find out how we collect, use and safeguard your personal information, please visit: " + privacy_en + ".\n\nIf you do not wish to receive Front Of The Line® Entertainment e-Update emails in the future, please click here: " + unsubscribe_en + ". This email address is not monitored for other inquiries and messages received cannot be responded to.";
-
-  var reg_fr = "*Tout achat doit être porté intégralement à une Carte American Express. Sous réserve des disponibilités, des modalités, des restrictions, des procédures de vérification et des frais applicables à l’événement et à la billetterie. Les billets et les forfaits peuvent ne pas être transférables et ne doivent pas être revendus. Il n’y aura aucun échange et aucun remboursement, sous réserve des obligations du marchand en vertu des lois applicables.\n\nPour les billets achetés par l’intermédiaire de Ticketmaster.ca, American Express a fixé une limite de deux commandes pour cette offre. Cette limite s’applique à toutes les Cartes associées à tous vos comptes American Express. Chaque commande peut également être assujettie à des limites de billets déterminées par des parties autres qu’American Express.\n\nMD: utilisée en vertu d’une licence accordée par American Express. © Banque Amex du Canada, 2020.\n\nEnvoyée par la Banque Amex du Canada, C.P. 3204, succursale F, Toronto (Ontario) M1W 3W7. Vous pouvez vérifier l’authenticité du présent courriel en communiquant avec nous ou en allant au: " + contact_us_fr + ".\n\nNotez que ce courriel a été envoyé à : Si ce message a été reçu à une autre adresse, c’est qu’il y a été réacheminé.\n\nPour savoir comment nous recueillons, utilisons et protégeons vos renseignements personnels, visitez le site: " + privacy_fr + ".\n\nSi, à l'avenir, vous ne voulez pas recevoir de courriels d'Avant-Première, cliquer ici: " + unsubscribe_fr + ". Il faut prévoir environ de deux à quatre semaines pour le traitement de votre demande. Nous ne vérifions pas les autres courriels envoyés à cette adresse électronique et nous n’y répondons pas.";
+  var reg_fr = "*Tout achat doit être porté intégralement à une Carte American Express. Sous réserve des disponibilités, des modalités, des restrictions, des procédures de vérification et des frais applicables à l’événement et à la billetterie. Les billets et les forfaits peuvent ne pas être transférables et ne doivent pas être revendus. Il n’y aura aucun échange et aucun remboursement, sous réserve des obligations du marchand en vertu des lois applicables.\n\nMD: utilisée en vertu d’une licence accordée par American Express. © Banque Amex du Canada, 2020.\n\nEnvoyée par la Banque Amex du Canada, C.P. 3204, succursale F, Toronto (Ontario) M1W 3W7. Vous pouvez vérifier l’authenticité du présent courriel en communiquant avec nous ou en allant au: " + contact_us_fr + ".\n\nNotez que ce courriel a été envoyé à : Si ce message a été reçu à une autre adresse, c’est qu’il y a été réacheminé.\n\nPour savoir comment nous recueillons, utilisons et protégeons vos renseignements personnels, visitez le site: " + privacy_fr + ".\n\nSi, à l'avenir, vous ne voulez pas recevoir de courriels d'Avant-Première, cliquer ici: " + unsubscribe_fr + ". Nous ne vérifions pas les autres courriels envoyés à cette adresse électronique et nous n'y répondons pas.";
 
   var bud_stage_en = "*Purchase must be charged in full to an American Express Card. Subject to availability and to event and ticketing terms, restrictions, verification procedures and fees. Tickets and packages may not be transferable and should not be resold. No refunds and no exchanges subject to merchant's obligations under applicable law.\n\nFor tickets purchased through Ticketmaster.ca, American Express has set a two-order limit for this offer. This limit applies across all Cards associated with all of your American Express accounts. Each order may also be subject to ticket limits set out by parties other than American Express.\n\n**Access to the American Express® Entrance is available to individuals upon the presentation of a valid American Express Card.\n\n***Subject to Lounge capacity.\n\n® Used under license from American Express. Copyright ©: AMEX Bank of Canada, 2020.\n\nSent from Amex Bank of Canada, P.O. Box 3204, Station 'F', Toronto, ON, M1W 3W7. You can contact us or verify with us the authenticity of this email by visiting: " + contact_us_en + ".\n\nNote that this e-mail was sent to: %%EMAIL%%. If you received this email at a different address, this email message was forwarded.\n\nTo find out how we collect, use and safeguard your personal information, please visit: " + privacy_en + ".\n\nIf you do not wish to receive Front Of The Line® Entertainment e-Update emails in the future, please click here: " + unsubscribe_en + ". This email address is not monitored for other inquiries and messages received cannot be responded to.";
+  var bud_stage_fr = "*Tout achat doit être porté intégralement à une Carte American Express. Sous réserve des disponibilités, des modalités, des restrictions, des procédures de vérification et des frais applicables à l’événement et à la billetterie. Les billets et les forfaits peuvent ne pas être transférables et ne doivent pas être revendus. Il n’y aura aucun échange et aucun remboursement, sous réserve des obligations du marchand en vertu des lois applicables.\n\nPour les billets achetés par l’intermédiaire de Ticketmaster.ca, American Express a fixé une limite de deux commandes pour cette offre. Cette limite s’applique à toutes les Cartes associées à tous vos comptes American Express. Chaque commande peut également être assujettie à des limites de billets déterminées par des parties autres qu’American Express.\n\nMD: utilisée en vertu d’une licence accordée par American Express. © Banque Amex du Canada, 2020.\n\nEnvoyée par la Banque Amex du Canada, C.P. 3204, succursale F, Toronto (Ontario) M1W 3W7. Vous pouvez vérifier l’authenticité du présent courriel en communiquant avec nous ou en allant au: " + contact_us_fr + ".\n\nNotez que ce courriel a été envoyé à : Si ce message a été reçu à une autre adresse, c’est qu’il y a été réacheminé.\n\nPour savoir comment nous recueillons, utilisons et protégeons vos renseignements personnels, visitez le site: " + privacy_fr + ".\n\nSi, à l'avenir, vous ne voulez pas recevoir de courriels d'Avant-Première, cliquer ici: " + unsubscribe_fr + ". Il faut prévoir environ de deux à quatre semaines pour le traitement de votre demande. Nous ne vérifions pas les autres courriels envoyés à cette adresse électronique et nous n’y répondons pas.";
 
-  var bud_stage_fr = "AAAA";
+  var bud_stage_blurb_en = "Enjoy special Cardmember benefits at Budweiser Stage including an exclusive American Express® Entrance**, and a chance to relax in the American Express® Lounge***.\n\n";
+  var bud_stage_blurb_fr = "Enjoy special Cardmember benefits at Budweiser Stage including an exclusive American Express® Entrance**, and a chance to relax in the American Express® Lounge***.\n\n";
 
   // array list of misc terms in both english and french, default is eng. When french is selected, it'll switch
   var misc_en = ["No Images? View this online", "AMERICAN EXPRESS invites®", "FRONT OF THE LINE® PRESALE", "Location & Venue", "More from American Express® Invites", "Membership Rewards", "My Card Benefits"];
@@ -96,6 +103,7 @@ $('#btnGenerate').click(function(e){
   var misc = misc_en;
 
   var lang = 'ENG';
+  var blurb = '';
   var terms = reg_en;
 
   // language conditional statement
@@ -107,16 +115,15 @@ $('#btnGenerate').click(function(e){
 
   // Budweiser stage conditional statement
   if ( $('#bud').prop('checked') == true ){
+    blurb = bud_stage_blurb_en;
     terms = bud_stage_en;
   }
 
   // Bud & french checked
   if ( $('#bud').prop('checked') == true && $('#french').prop('checked') == true ){
+    blurb = bud_stage_blurb_fr;
     terms = bud_stage_fr;
   }
-
-  // if img name is blank, give it the FPO name
-  if ( img_name == '' ){ img_name = 'AMEX_img_FPO.jpg'; }
 
   /*--------------------------------------------------------------
   ## Validator
@@ -137,26 +144,33 @@ $('#btnGenerate').click(function(e){
   }
 
   /*--------------------------------------------------------------
-  ## Replacement function
+  ## Replacement functions
   --------------------------------------------------------------*/
-  // finds name in presale and bolds it, replaces ® to HTML code
-  // var regx = new RegExp(name, 'g');
-  var newpresale = presale.replace(/[Á]/g, '&Aacute;').replace(/[á]/g, '&aacute;').replace(/[À]/g, '&Agrave;').replace(/[à]/g, '&agrave;').replace(/[Â]/g, '&Acirc;').replace(/[â]/g, '&acirc;').replace(/[É]/g, '&Eacute;').replace(/[é]/g, '&eacute;').replace(/[È]/g, '&Egrave;').replace(/[è]/g, '&egrave;').replace(/[Ê]/g, '&Ecirc;').replace(/[ê]/g, '&ecirc;').replace(/[Í]/g, '&Iacute;').replace(/[í]/g, '&iacute;').replace(/[Ì]/g, '&Igrave;').replace(/[ì]/g, '&igrave;').replace(/[Î]/g, '&Icirc;').replace(/[î]/g, '&icirc;').replace(/[Ó]/g, '&Oacute;').replace(/[ó]/g, '&oacute;').replace(/[Ò]/g, '&Ograve;').replace(/[ò]/g, '&ograve;').replace(/[Ô]/g, '&Ocirc;').replace(/[ô]/g, '&ocirc;').replace(/[Ú]/g, '&Uacute;').replace(/[ú]/g, '&uacute;').replace(/[Ù]/g, '&Ugrave;').replace(/[ù]/g, '&ugrave;').replace(/[Û]/g, '&Ucirc;').replace(/[û]/g, '&ucirc;').replace(/[Ç]/g, '&Ccedil;').replace(/[ç]/g, '&ccedil;').replace(/®/g, '<sup style="font-size:10px;">&reg;</sup>').replace(/©/g, '<sup style="font-size:10px;">&copy;</sup>').replace(/“/g, '&ldquo;').replace(/”/g, '&rdquo;').replace(/\n/g, '<br>\r');
+  // array list of cities
+  var arr = ["Toronto", "Hamilton", "Montreal", "Laval", "Vancouver", "Edmonton", "Saskatoon"];
+  
+  // assign to RegExp for global changes
+  var city1 = new RegExp(arr[0], 'g');
+  var city2 = new RegExp(arr[1], 'g');
+  var city3 = new RegExp(arr[2], 'g');
+  var city4 = new RegExp(arr[3], 'g');
+  var city5 = new RegExp(arr[4], 'g');
+  var city6 = new RegExp(arr[5], 'g');
+  var city7 = new RegExp(arr[6], 'g');
 
-  var newLocation = location.replace(/[Á]/g, '&Aacute;').replace(/[á]/g, '&aacute;').replace(/[À]/g, '&Agrave;').replace(/[à]/g, '&agrave;').replace(/[Â]/g, '&Acirc;').replace(/[â]/g, '&acirc;').replace(/[É]/g, '&Eacute;').replace(/[é]/g, '&eacute;').replace(/[È]/g, '&Egrave;').replace(/[è]/g, '&egrave;').replace(/[Ê]/g, '&Ecirc;').replace(/[ê]/g, '&ecirc;').replace(/[Í]/g, '&Iacute;').replace(/[í]/g, '&iacute;').replace(/[Ì]/g, '&Igrave;').replace(/[ì]/g, '&igrave;').replace(/[Î]/g, '&Icirc;').replace(/[î]/g, '&icirc;').replace(/[Ó]/g, '&Oacute;').replace(/[ó]/g, '&oacute;').replace(/[Ò]/g, '&Ograve;').replace(/[ò]/g, '&ograve;').replace(/[Ô]/g, '&Ocirc;').replace(/[ô]/g, '&ocirc;').replace(/[Ú]/g, '&Uacute;').replace(/[ú]/g, '&uacute;').replace(/[Ù]/g, '&Ugrave;').replace(/[ù]/g, '&ugrave;').replace(/[Û]/g, '&Ucirc;').replace(/[û]/g, '&ucirc;').replace(/[Ç]/g, '&Ccedil;').replace(/[ç]/g, '&ccedil;').replace(/®/g, '<sup style="font-size:10px;">&reg;</sup>').replace(/©/g, '<sup style="font-size:10px;">&copy;</sup>').replace(/“/g, '&ldquo;').replace(/”/g, '&rdquo;').replace(/\n/g, '<br>\r');
+  // replace all instances
+  var locationTxt = location
+  .replace(city1, arr[0].toString().toUpperCase())
+  .replace(city2, arr[1].toString().toUpperCase())
+  .replace(city3, arr[2].toString().toUpperCase())
+  .replace(city4, arr[3].toString().toUpperCase())
+  .replace(city5, arr[4].toString().toUpperCase())
+  .replace(city6, arr[5].toString().toUpperCase())
+  .replace(city7, arr[6].toString().toUpperCase())
 
   /*--------------------------------------------------------------
   ## Location & Venue
   --------------------------------------------------------------*/
-  // Gets first line, capitalizes it
-  var firstLn = location.split('\n');
-  var cap = firstLn[0].toUpperCase();
-  var firstLnFrmt = '<b>' + cap + '</b><br>';
-
-  // Removes the first line from location input because its already saved in from firstLnFrmt
-  var rmFirstLn = location.replace(firstLn[0], '');
-
-  var locationFinal = firstLnFrmt + rmFirstLn;
 
   /*--------------------------------------------------------------
   ## Mailing ID
@@ -182,16 +196,33 @@ $('#btnGenerate').click(function(e){
   /*--------------------------------------------------------------
   ## Output
   --------------------------------------------------------------*/
-  $('#outputHtml').val(
-    name + '\n\r' +
-    img_name + '\n\r' +
-    newpresale + '\n\r' +
-    locationFinal
-  );
+  /*--------------------------------------------------------------
+  ### HTML
+  --------------------------------------------------------------*/
+  // add bold tags fon every city replacement
+  var locationHtml = location
+  .replace(city1, '<b>' + arr[0].toString().toUpperCase() + '</b>')
+  .replace(city2, '<b>' + arr[1].toString().toUpperCase() + '</b>')
+  .replace(city3, '<b>' + arr[2].toString().toUpperCase() + '</b>')
+  .replace(city4, '<b>' + arr[3].toString().toUpperCase() + '</b>')
+  .replace(city5, '<b>' + arr[4].toString().toUpperCase() + '</b>')
+  .replace(city6, '<b>' + arr[5].toString().toUpperCase() + '</b>')
+  .replace(city7, '<b>' + arr[6].toString().toUpperCase() + '</b>')
+  .replace(/[Á]/g, '&Aacute;').replace(/[á]/g, '&aacute;').replace(/[À]/g, '&Agrave;').replace(/[à]/g, '&agrave;').replace(/[Â]/g, '&Acirc;').replace(/[â]/g, '&acirc;').replace(/[É]/g, '&Eacute;').replace(/[é]/g, '&eacute;').replace(/[È]/g, '&Egrave;').replace(/[è]/g, '&egrave;').replace(/[Ê]/g, '&Ecirc;').replace(/[ê]/g, '&ecirc;').replace(/[Í]/g, '&Iacute;').replace(/[í]/g, '&iacute;').replace(/[Ì]/g, '&Igrave;').replace(/[ì]/g, '&igrave;').replace(/[Î]/g, '&Icirc;').replace(/[î]/g, '&icirc;').replace(/[Ó]/g, '&Oacute;').replace(/[ó]/g, '&oacute;').replace(/[Ò]/g, '&Ograve;').replace(/[ò]/g, '&ograve;').replace(/[Ô]/g, '&Ocirc;').replace(/[ô]/g, '&ocirc;').replace(/[Ú]/g, '&Uacute;').replace(/[ú]/g, '&uacute;').replace(/[Ù]/g, '&Ugrave;').replace(/[ù]/g, '&ugrave;').replace(/[Û]/g, '&Ucirc;').replace(/[û]/g, '&ucirc;').replace(/[Ç]/g, '&Ccedil;').replace(/[ç]/g, '&ccedil;').replace(/®/g, '<sup style="font-size:10px;">&reg;</sup>').replace(/©/g, '<sup style="font-size:10px;">&copy;</sup>').replace(/“/g, '&ldquo;').replace(/”/g, '&rdquo;').replace(/\n/g, '<br>\r');
 
-  $('#outputTxt').val(
-    misc[0] + ":\nhttp://%%track%%/amex2?t_sparams=%%t_sparams%%\n\n" + misc[1] + ": " + ae_invites_en + "\n\n" + misc[2] + "\n" + name.toUpperCase() + "\n\n" + presale + "\n" + misc[3] + "\n\n" + location + "\n\n" + misc[4] + ": " + ae_invites_en + "\n\n" + misc[5] + ": " + membership_rewards_en + "\n\n" + misc[6] + ": " + card_benefits_en + "\n\n" + terms
-  );
+
+  var strHtml = misc[0] + ":\nhttp://%%track%%/amex2?t_sparams=%%t_sparams%%\n\n" + misc[1] + ": " + ae_invites_en + "\n\n" + img_link + "\n\n" + misc[2] + "\n" + name.toUpperCase() + "\n\n" + presale + '\n\n' + blurb + misc[3] + "\n\n" + locationHtml + "\n\n" + misc[4] + ": " + ae_invites_en + "\n\n" + misc[5] + ": " + membership_rewards_en + "\n\n" + misc[6] + ": " + card_benefits_en + "\n\n" + terms
+  var strHtmlFinal = strHtml
+  .replace(/[Á]/g, '&Aacute;').replace(/[á]/g, '&aacute;').replace(/[À]/g, '&Agrave;').replace(/[à]/g, '&agrave;').replace(/[Â]/g, '&Acirc;').replace(/[â]/g, '&acirc;').replace(/[É]/g, '&Eacute;').replace(/[é]/g, '&eacute;').replace(/[È]/g, '&Egrave;').replace(/[è]/g, '&egrave;').replace(/[Ê]/g, '&Ecirc;').replace(/[ê]/g, '&ecirc;').replace(/[Í]/g, '&Iacute;').replace(/[í]/g, '&iacute;').replace(/[Ì]/g, '&Igrave;').replace(/[ì]/g, '&igrave;').replace(/[Î]/g, '&Icirc;').replace(/[î]/g, '&icirc;').replace(/[Ó]/g, '&Oacute;').replace(/[ó]/g, '&oacute;').replace(/[Ò]/g, '&Ograve;').replace(/[ò]/g, '&ograve;').replace(/[Ô]/g, '&Ocirc;').replace(/[ô]/g, '&ocirc;').replace(/[Ú]/g, '&Uacute;').replace(/[ú]/g, '&uacute;').replace(/[Ù]/g, '&Ugrave;').replace(/[ù]/g, '&ugrave;').replace(/[Û]/g, '&Ucirc;').replace(/[û]/g, '&ucirc;').replace(/[Ç]/g, '&Ccedil;').replace(/[ç]/g, '&ccedil;').replace(/®/g, '<sup style="font-size:10px;">&reg;</sup>').replace(/©/g, '<sup style="font-size:10px;">&copy;</sup>').replace(/“/g, '&ldquo;').replace(/”/g, '&rdquo;').replace(/\n/g, '<br>\r');
+
+  $('#outputHtml').val(strHtmlFinal);
+
+  /*--------------------------------------------------------------
+  ### TXT
+  --------------------------------------------------------------*/
+  var strTxtFinal = misc[0] + ":\nhttp://%%track%%/amex2?t_sparams=%%t_sparams%%\n\n" + misc[1] + ": " + ae_invites_en + "\n\n" + misc[2] + "\n" + name.toUpperCase() + "\n\n" + presale + '\n\n' + blurb + misc[3] + "\n\n" + locationTxt + "\n\n" + misc[4] + ": " + ae_invites_en + "\n\n" + misc[5] + ": " + membership_rewards_en + "\n\n" + misc[6] + ": " + card_benefits_en + "\n\n" + terms
+  
+  $('#outputTxt').val(strTxtFinal);
 });
 
 /*--------------------------------------------------------------
